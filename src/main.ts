f788166d307f84511b98a2944aa71b3723a944a9
Jens Ungerer <github@jens-ungerer.de>
2020-07-05 12:09:03 +0200

    fixing linting errors

--- src/main.ts
+++ src/main.ts
@@ -1,58 +1,58 @@
 import path from 'path';
 import { argv, exit } from 'process';
-
 import { NpmDependenciesBundler } from './npmDependenciesBundler';
-
+import { Logger } from './app/common/logger';
 
 if (!argv || argv.length < 4) {
-    console.error('Usage:');
+  Logger.log('Usage:');
+  // console.error('Usage:');
 } else {
-    const npmDependencyBundler = new NpmDependenciesBundler();
+  const npmDependencyBundler = new NpmDependenciesBundler();
 
-    const absolutePathToExecutable = argv[1];
-    const command = argv[2];
-    const relativePathToDependenciesFolder = '.\\.' + argv[3];
-    const currentDirRelativePath = '.\\..\\..\\';
-    // DEBUGGING
-    // console.log(relativePathToDependenciesFolder);
+  const absolutePathToExecutable = argv[1];
+  const command = argv[2];
+  const relativePathToDependenciesFolder = '.\\.' + argv[3];
+  const currentDirRelativePath = '.\\..\\..\\';
+  // DEBUGGING
+  // console.log(relativePathToDependenciesFolder);
 
-    const prepareDownload = async () => {
-        const absolutePathToDependencies = path.resolve(absolutePathToExecutable, relativePathToDependenciesFolder);
-        await npmDependencyBundler.prepareDownload(absolutePathToDependencies);
-    };
+  const prepareDownload = async () => {
+    const absolutePathToDependencies = path.resolve(absolutePathToExecutable, relativePathToDependenciesFolder);
+    await npmDependencyBundler.prepareDownload(absolutePathToDependencies);
+  };
 
-    const download = async () => {
-        const currentDirAbsolutePath = path.resolve(absolutePathToExecutable, currentDirRelativePath);
-        await npmDependencyBundler.download(currentDirAbsolutePath);
-    };
+  const download = async () => {
+    const currentDirAbsolutePath = path.resolve(absolutePathToExecutable, currentDirRelativePath);
+    await npmDependencyBundler.download(currentDirAbsolutePath);
+  };
 
-    const prepareInstall = async () => {
+  const prepareInstall = async () => {
 
-    };
+  };
 
-    const install = async () => {
+  const install = async () => {
 
-    };
+  };
 
-    const startCommand = async (cliCommand: string) => {
-        switch (cliCommand) {
-            case 'download': {
-                await prepareDownload();
-                await download()
-                break;
-            }
-            case 'install': {
-                await prepareInstall();
-                await install();
-                break;
-            }
-            default: {
-                break;
-            }
-        }
-    };
+  const startCommand = async (cliCommand: string) => {
+    switch (cliCommand) {
+      case 'download': {
+        await prepareDownload();
+        await download();
+        break;
+      }
+      case 'install': {
+        await prepareInstall();
+        await install();
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  };
 
-    startCommand(command);
+  startCommand(command);
 
-    exit();
+  exit();
 }
--- src/typings/typings.d.ts
+++ src/typings/typings.d.ts
@@ -1,2 +1,3 @@
 declare const VERSION: string;
 declare const DEVELOP: boolean;
+// declare const npm: any;
